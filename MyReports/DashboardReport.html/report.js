6bd7ba2b80cb3ae24ef032782560caeb1150cc41
Akhil1212 <akhilgelli1141@gmail.com>
2020-08-25 17:52:10 +0530

    firstcommit

---
+++ MyReports/DashboardReport.html/report.js
@@ -0,0 +1,68 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("Include/features/proAcc_Featurefile/Home_Page/Dashboard.feature");
+formatter.feature({
+  "name": "To view all the details",
+  "description": "",
+  "keyword": "Feature"
+});
+formatter.scenarioOutline({
+  "name": "Login to application",
+  "description": "",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "name": "@SmokeTest"
+    }
+  ]
+});
+formatter.step({
+  "name": "User should navigate to ProAcc page",
+  "keyword": "Given "
+});
+formatter.step({
+  "name": "User enters \u003cusername\u003e and \u003cpassword\u003e",
+  "keyword": "And "
+});
+formatter.step({
+  "name": "User clicks on LOG IN button",
+  "keyword": "When "
+});
+formatter.step({
+  "name": "User will be logged in successfully",
+  "keyword": "Then "
+});
+formatter.examples({
+  "name": "",
+  "description": "",
+  "keyword": "Examples",
+  "rows": [
+    {
+      "cells": [
+        "username",
+        "password"
+      ]
+    },
+    {
+      "cells": [
+        "Tin_PM",
+        "a12345@"
+      ]
+    }
+  ]
+});
+formatter.scenario({
+  "name": "Login to application",
+  "description": "",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "name": "@SmokeTest"
+    }
+  ]
+});
+formatter.step({
+  "name": "User should navigate to ProAcc page",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "LogintoProAcc_StepDef.user_should_navigate_to_ProAcc_page()"
+});
